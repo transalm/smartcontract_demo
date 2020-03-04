04398278ba71c2f51350cf2edee109b8d333b737
dblanch1982 <daina.blanch@gmail.com>
2020-03-04 10:06:33 +0100

    Code mit Alter unter 50 Jahren
    Code aktualisiert mit der Bedingung --> Alter unter 50 Jahren

--- srcs/solidity/LifeDeath.sol
+++ srcs/solidity/LifeDeath.sol
@@ -1,75 +1,80 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract InsuranceContract {
-
-    struct PolicyHolder {
+    
+    struct PolicyContract {
+        int dateOfBirth;
         address policyHolderAddress;
-    }
-    
-    struct Beneficiary {
-        uint idRequest;
-        uint amount;
         address beneficiaryAddress;
+        uint refund;
+        uint policyID;
+        uint premium;
     }
 
-    address public manager;
-    Beneficiary public beneficiary;
-    PolicyHolder policyHolder;
+    address public insurer;
     uint requestID;
-    Beneficiary[] beneficiaryList;
-
-    mapping(address => PolicyHolder) public policyHolders;
-    
-    mapping(address => Beneficiary[]) public beneficiaries;
-    
-    address [] public senderList;
+    PolicyContract pc;
+    PolicyContract[] public policyContractList;
 
     constructor() public {
-       manager = msg.sender;
+       insurer = msg.sender;
     }
     
-    function addRequest(
-        address  addressBeneficiary
+    function underwrite (
+        int dateOfBirthInsured, 
+        address policyHoldersAddress,
+        address beneficiaryAddress,
+        uint premium
         ) 
-        public payable 
-        returns (uint idRequest)
+        public payable restricted 
+         returns (uint idRequest)
         {
-        
+            
         require(msg.value > .01 ether);
-        policyHolders[ msg.sender ].policyHolderAddress = msg.sender;
-        
-        beneficiary.amount = msg.value;
-        beneficiary.beneficiaryAddress = addressBeneficiary;
+
+        pc.refund = msg.value;
+        pc.dateOfBirth = dateOfBirthInsured;
+        pc.beneficiaryAddress = beneficiaryAddress;
+        pc.policyHolderAddress = policyHoldersAddress;
+        pc.premium = premium;
         requestID = requestID + 1;
-        beneficiary.idRequest = requestID;
-        beneficiaries[ msg.sender ].push(beneficiary);
-        
-        
-        bool isFound = false;
-        for (uint i=0; i<senderList.length; i++) { 
-            if (senderList[i] == msg.sender) {
-                isFound = true;
-            }
-        }
-        if (!isFound) {
-            senderList.push(msg.sender);
-        }
+        pc.policyID = requestID;
+        policyContractList.push(pc);
         
         return requestID;
     }
     
     modifier restricted() {
-        require(msg.sender == manager);
+        require(msg.sender == insurer);
         _;
     }
 
-    function payAmountProcess(address policyHolderAddr) public restricted{
-        beneficiaryList = beneficiaries[policyHolderAddr];
-        uint arrayLength = beneficiaryList.length;
-        for (uint i=0; i<arrayLength; i++) {
-            beneficiaryList[i].beneficiaryAddress.transfer(beneficiaryList[i].amount);
+    function getPolicyContract(uint policyID) public view returns (uint, address, address, int, uint){
+        for (uint i=0; i<policyContractList.length; i++) { 
+            if (policyContractList[i].policyID == policyID) {
+                return (policyContractList[i].policyID , policyContractList[i].policyHolderAddress, policyContractList[i].beneficiaryAddress, policyContractList[i].dateOfBirth, policyContractList[i].refund );
+            }
         }
-        delete beneficiaries[policyHolderAddr];
-        delete policyHolders[policyHolderAddr];
+        return (0, 0x0, 0x0, 0, 0);
     }
+
+    function claim(uint policyID, int dateOfDeath) public restricted{
+        
+        for (uint i=0; i<policyContractList.length; i++) { 
+            if (policyContractList[i].policyID == policyID) {
+                
+                int startDate = policyContractList[i].dateOfBirth; 
+                int endDate = dateOfDeath; 
+
+                int diff = (endDate - startDate) / 60 / 60 / 24 / 365; 
+                
+                if (diff < 50) {
+                    policyContractList[i].beneficiaryAddress.transfer(policyContractList[i].refund);
+                } else {
+                    insurer.transfer(policyContractList[i].refund);
+                }
+                policyContractList[i].refund = 0;
+            }
+        }
+    } 
 }
